9c155414e87c82e085e5c3300f19402b329e4643
akshit-yadav <akshit.crown@gmail.com>
2023-12-11 01:52:23 +0530

    initial commit

---
+++ employee_project/employee_register/urls.py
@@ -0,0 +1,11 @@
+from this import d
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.employee_form, name='employee_insert'),
+    path('<int:id>/', views.employee_form, name='employee_update'),
+    path('list/', views.employee_list, name='employee_list'),
+    path('delete/<int:id>/',views.employee_delete, name='employee_delete'),
+    
+]
